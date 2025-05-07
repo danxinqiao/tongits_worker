8d5cfe89453ee1ba540912e4da0f2e606a0b8f18
xinqiao <1090920848@qq.com>
2025-05-07 14:12:18 +0800

    使用地址跳转的方式下载

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -7,19 +7,5 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 const downUrl = "/api/downApk";
 
-const downloadFile = () => {
-  const iframe = document.createElement("iframe");
-  iframe.style.display = "none";
-  iframe.src = downUrl;
-  document.body.appendChild(iframe);
-
-  iframe.onload = () => {
-    router.push("/");
-    document.body.removeChild(iframe);
-  };
-};
-
-onMounted(() => {
-  downloadFile();
-});
+router.push(downUrl);
 </script>
