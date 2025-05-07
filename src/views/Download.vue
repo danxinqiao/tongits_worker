93706635cd10927728faf7446dd7fbe64a86e73f
xinqiao <1090920848@qq.com>
2025-05-07 16:16:18 +0800

    使用a标签打开新的页面

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -1,18 +1,43 @@
-<template></template>
+<template>
+  <div>
+    <a
+      ref="downloadLink"
+      :href="downUrl"
+      download="TongitsPinoy.apk"
+      style="display: none"
+      target="_blank"
+      >Download APK</a
+    >
+  </div>
+</template>
 
 <script setup>
-import { onMounted } from "vue";
+// import { onMounted } from "vue";
+// import { useRouter } from "vue-router";
+
+// const router = useRouter();
+// const downUrl = "/api/downApk";
+
+// const downloadFile = () => {
+//   window.location.href = downUrl;
+//   setTimeout(() => router.push("/"), 3000);
+// };
+
+// onMounted(() => {
+//   downloadFile();
+// });
+import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 
-const router = useRouter();
 const downUrl = "/api/downApk";
-
-const downloadFile = () => {
-  window.location.href = downUrl;
-  setTimeout(() => router.push("/"), 3000);
-};
-
+const downloadLink = ref(null);
 onMounted(() => {
-  downloadFile();
+  if (downloadLink.value) {
+    downloadLink.value.click();
+  }
+  const router = useRouter();
+  setTimeout(() => {
+    router.push("/");
+  }, 6000);
 });
 </script>
