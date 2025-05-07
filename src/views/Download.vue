bb88a5de51fe259dec112f22dd66ab6ca4fac3ec
xinqiao <1090920848@qq.com>
2025-05-07 17:05:48 +0800

    修改api下载的名称

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -11,25 +11,10 @@
 </template>
 
 <script setup>
-// import { onMounted } from "vue";
-// import { useRouter } from "vue-router";
-
-// const router = useRouter();
-// const downUrl = "/api/downApk";
-
-// const downloadFile = () => {
-//   window.location.href = downUrl;
-//   setTimeout(() => router.push("/"), 3000);
-// };
-
-// onMounted(() => {
-//   downloadFile();
-// });
 import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 
-// const downUrl = "/api/downApk";
-const downUrl = import.meta.env.VITE_DOWN_OFFICIAL_APK;
+const downUrl = "/api/downloadApk";
 const downloadLink = ref(null);
 onMounted(() => {
   if (downloadLink.value) {
