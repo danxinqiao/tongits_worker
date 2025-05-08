ba81886b1f4cd4bb6a6f3ecac2e657432abfe7fc
xinqiao <1090920848@qq.com>
2025-05-08 09:01:37 +0800

    download页面增加安卓和手机端的判断

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -16,10 +16,27 @@ import { useRouter } from "vue-router";
 
 const downUrl = "/api/downloadApk";
 const downloadLink = ref(null);
+const isIOS = false;
+const isMobile = false;
+
+checkDevice(() => {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  // 检测iOS设备
+  isIOS.value = /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
+  // 检测移动端设备
+  isMobile.value =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      userAgent,
+    );
+});
+
 onMounted(() => {
-  if (downloadLink.value) {
-    downloadLink.value.click();
+  if (isMobile.value && !isIOS.value) {
+    if (downloadLink.value) {
+      downloadLink.value.click();
+    }
   }
+
   const router = useRouter();
   setTimeout(() => {
     router.push("/");
