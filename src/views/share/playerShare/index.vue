03871af7d8b84de866aa86ecb611c8b354b76e1b
xinqiao <1090920848@qq.com>
2025-07-07 17:38:37 +0800

    测试5

--- src/views/share/playerShare/index.vue
+++ src/views/share/playerShare/index.vue
@@ -167,8 +167,8 @@ const isFacebookApp = () => {
 const goToDownload = () => {
   const fallbackUrl = window.location.origin;
   if (isFacebookApp()) {
-    location.href = "https://tongits-worker.pages.dev/home";
-    alert("------------------");
+    window.top.location.href = "https://tongits-worker.pages.dev";
+    alert("11111111111111");
   } else {
     if (navigator.userAgent.match(/iphone|ipad|ipod/i)) {
       location.href = fallbackUrl; // 'https://apps.apple.com/us/app/id1507313633'
